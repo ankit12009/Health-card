40716fce879e54e017bf5a92fae3bb5159a041b0
Ankit Kharwar <101902489+ankit12009@users.noreply.github.com>
2024-11-03 19:35:46 +0530

    login php added

---
+++ login.php
@@ -0,0 +1,29 @@
+ <?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "admin";
+    $con = mysqli_connect($server, $user, $password, $db);
+
+    if (isset($_POST['submit'])){
+        $username = $_POST['Username'];
+        $password = $_POST['password'];
+        $sql = "SELECT * FROM user WHERE username='$username' AND pw='$password'";
+        $result = $con->query($sql);
+        if ($result->num_rows > 0) {
+            $admin="Admin";
+            $apass="admin1209";
+            if ($username === $admin && $password === $apass) {
+                echo "<script>alert('Hello Admin sir');</script>";
+                exit();
+            }
+             else{
+                echo "<script>alert('Login successful!');</script>";
+                header('location:index.html');
+             }
+        }
+         else {
+            echo "<script>alert('Invalid username or password');</script>";
+        }
+    }
+    ?>
